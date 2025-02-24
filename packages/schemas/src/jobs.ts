0059f7b2ef28af95e64dc8d957c6805e84c899b9
Derrick Nnaji <dnnaji@gmail.com>
2025-02-23 18:51:29 -0600

    fix slow types

--- packages/schemas/src/jobs.ts
+++ packages/schemas/src/jobs.ts
@@ -1,7 +1,8 @@
 import { pgTable, text, boolean, json, timestamp } from "drizzle-orm/pg-core";
+import type { PgTableWithColumns } from "drizzle-orm/pg-core";
 
 // New table to store managed jobs
-export const jobs = pgTable("jobs", {
+export const jobs: PgTableWithColumns<unknown> = pgTable("jobs", {
   id: text("id").primaryKey().notNull(), // using a string ID
   name: text("name").notNull(),
   active: boolean("active").notNull().default(true),
