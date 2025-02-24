87ded4809e25640111ecf093e138ed7360fb107c
Derrick Nnaji <dnnaji@gmail.com>
2025-02-23 19:00:15 -0600

    type tweak

--- packages/schemas/src/jobs.ts
+++ packages/schemas/src/jobs.ts
@@ -2,7 +2,7 @@ import { pgTable, text, boolean, json, timestamp } from "drizzle-orm/pg-core";
 import type { PgTableWithColumns } from "drizzle-orm/pg-core";
 
 // New table to store managed jobs
-export const jobs: PgTableWithColumns<unknown> = pgTable("jobs", {
+export const jobs: PgTableWithColumns<any> = pgTable("jobs", {
   id: text("id").primaryKey().notNull(), // using a string ID
   name: text("name").notNull(),
   active: boolean("active").notNull().default(true),
